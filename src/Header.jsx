e8229c370a99de244e250c03cd8ffa4eb9e2ca62
Guardabrazo <guardabrazo@Guardabrazos-MacBook-Pro.local>
2025-05-15 14:12:24 +0200

    Minor tweaks

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,10 +4,10 @@ import './Header.css'; // We'll create this CSS file next for styling
 function Header({ onInfoClick }) { // Added onInfoClick prop
   return (
     <header className="app-header">
-      <div className="header-name">GUARDABRAZO</div>
+      <div className="header-name">guardabrazo</div>
       <nav className="header-nav">
         {/* Replaced About/Contact with Info button, removed pill class */}
-        <button onClick={onInfoClick} className="header-info-button header-pill-link">Info</button>
+        <button onClick={onInfoClick} className="header-info-button header-pill-link">info</button>
       </nav>
     </header>
   );
